dd08a42a82c0c8ff5599ba33dc209265fd286d9c
koliaay <koliaay@gmail.com>
2022-07-07 17:23:35 +0300

    MENU

--- OLX/ClientApp/src/components/Home/index.tsx
+++ OLX/ClientApp/src/components/Home/index.tsx
@@ -6,9 +6,137 @@ const HomePage = () => {
       <Helmet>
         <title>Головна</title>
       </Helmet>
-     
-      <h1 className="text-center">сівй текст</h1>
-      
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3 rounded-3 border border-warning">
+            <div className="accordion" id="accordionPanelsStayOpenExample">
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="panelsStayOpen-headingOne">
+                  <button
+                    className="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseOne"
+                    aria-expanded="true"
+                    aria-controls="panelsStayOpen-collapseOne"
+                  >
+                    Категорії
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseOne"
+                  className="accordion-collapse collapse show"
+                  aria-labelledby="panelsStayOpen-headingOne"
+                >
+                  <div className="accordion-body">
+                    Підгрупи розділу
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="panelsStayOpen-collapseTwo"
+                  >
+                    Фільтр
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseTwo"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="panelsStayOpen-headingTwo"
+                >
+                  <div className="accordion-body">
+                  Підгрупи розділу
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="panelsStayOpen-headingThree">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseThree"
+                    aria-expanded="false"
+                    aria-controls="panelsStayOpen-collapseThree"
+                  >
+                    Обране
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseThree"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="panelsStayOpen-headingThree"
+                >
+                  <div className="accordion-body">
+                  Підгрупи розділу
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="panelsStayOpen-headingFour">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseFour"
+                    aria-expanded="false"
+                    aria-controls="panelsStayOpen-collapseFour"
+                  >
+                   Стан
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseFour"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="panelsStayOpen-headingFour"
+                >
+                  <div className="accordion-body">
+                  Підгрупи розділу
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="panelsStayOpen-headingFive">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#panelsStayOpen-collapseFive"
+                    aria-expanded="false"
+                    aria-controls="panelsStayOpen-collapseFive"
+                  >
+                   Колір
+                  </button>
+                </h2>
+                <div
+                  id="panelsStayOpen-collapseFive"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="panelsStayOpen-headingFive"
+                >
+                  <div className="accordion-body">
+                  Підгрупи розділу
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-9">
+            <img
+              src="src\components\Home\Img\baner.png"
+              className="img-fluid rounded-3"
+              alt="baner"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
