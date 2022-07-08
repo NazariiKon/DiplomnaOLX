2d4e91786b97b04a59975f8a0ce6d5cdb0f41f99
Nazar <nazar.konechniy2@gmail.com>
2022-07-08 10:07:41 +0300

    fix img

--- OLX/ClientApp/src/components/containers/DefaultLayout/DefaultFooter.tsx
+++ OLX/ClientApp/src/components/containers/DefaultLayout/DefaultFooter.tsx
@@ -1,4 +1,10 @@
 import "./footer.css";
+import inst from '../../../images/icon/inst.png';
+import fb from '../../../images/icon/fb.png';
+import app_store from '../../../images/icon/google_play.png';
+import google_play from '../../../images/icon/app_store.png';
+
+
 const DefaultFooter = () => {
   return (
 
@@ -6,17 +12,17 @@ const DefaultFooter = () => {
       <section className="">
         <div className="container text-center text-md-start mt-5">
           <div className="row mt-3">
-            <div className="col-md-5 col-lg-5 col-xl-5 mx-auto mb-4">
+            <div className="col-md-6 col-lg-6 col-xl-6 mx-auto mb-4">
               <h6 className="footer-company">
                 @all.in.ua
               </h6>
               <p className="footer-name">
                 Одяг, взуття, аксесуари за доступною ціною. Всі права захищені.
-              </p>
+              </p>  
               <p>
-                  <a href="#!"><img src="src\components\containers\DefaultLayout\Img\app_store.png"></img></a>
-                  <a href="#!"><img src="src\components\containers\DefaultLayout\Img\google_play.png"></img></a>
-                </p>
+                  <img className="footer-logo" src={app_store}></img>
+                  <img className="footer-logo" src={google_play}></img>
+              </p>
             </div>
 
             <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mb-4">
@@ -35,8 +41,8 @@ const DefaultFooter = () => {
               <h6 className="footer-ref">
                 Ми в соц. мережах
                 <p>
-                  <a href="#!"><img src="src\components\containers\DefaultLayout\Img\inst.png"></img></a>
-                  <a href="#!"><img src="src\components\containers\DefaultLayout\Img\fb.png"></img></a>
+                  <a href="#!"><img src={fb}></img></a>
+                  <a href="#!"><img src={inst}></img></a>
                 </p>
               </h6>
             </div>
